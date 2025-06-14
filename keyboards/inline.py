53083d63554ecc1968046dabb8c7f810f7e6b14c
vandal_325 <i_shkryl@mail.ru>
2025-06-14 16:01:31 +0300

    Add keyboards

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,18 +1,29 @@
 from aiogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 
+
 help_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text="Поддержка🛠️",url="https://t.me/vandal_325")
+            InlineKeyboardButton(text="🛠️Поддержка🛠️",url="https://t.me/vandal_325")
         ]
     ]
 )
 
-# start_keyboard = InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         [
-#             InlineKeyboardButton(text="Скачать видео▶️",url="https://www.youtube.com/")
-#             InlineKeyboardButton(text)
-#         ]
-#     ]
-# )
+start_keyboard = InlineKeyboardMarkup(
+     inline_keyboard=[
+         [
+             InlineKeyboardButton(text="️▶️Скачать видео",callback_data="download_pressed"),
+             InlineKeyboardButton(text="🆔Получить ID",callback_data="id_pressed")
+         ]
+
+     ]
+ )
+
+
+escape_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад❌",callback_data="escape_pressed")
+        ]
+    ]
+)
---
+++ keyboards/reply.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
+
+format_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="MP4▶️")],
+        [KeyboardButton(text="MP3▶️")]
+    ],
+    resize_keyboard=True
+)
