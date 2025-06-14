f1ad058a4004bdc79d28a72ecc7ea6c2416939cd
vandal_325 <i_shkryl@mail.ru>
2025-06-14 16:02:39 +0300

    Add router

--- downloader_bot.py
+++ downloader_bot.py
@@ -1,10 +1,13 @@
 from aiogram import Dispatcher, Bot
 from config import TOKEN
 import asyncio
+
+from handlers.callbacks import callback_router
 from handlers.commands import command_router
 
 dp = Dispatcher()
 dp.include_router(command_router)
+dp.include_router(callback_router)
 
 async def main():
     bot = Bot(token=TOKEN)
