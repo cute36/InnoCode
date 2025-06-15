f059999ae46fc1b885d50ba6b53e59656431111f
vandal_325 <i_shkryl@mail.ru>
2025-06-15 15:21:44 +0300

    Update menu buttons

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -13,8 +13,17 @@ start_keyboard = InlineKeyboardMarkup(
      inline_keyboard=[
          [
              InlineKeyboardButton(text="️▶️Скачать видео",callback_data="download_pressed"),
-             InlineKeyboardButton(text="🆔Получить ID",callback_data="id_pressed")
+             InlineKeyboardButton(text="🆔Получить ID",callback_data="id_pressed"),
+         ],
+         [
+             InlineKeyboardButton(text="🖐Наш канал",url="https://t.me/uroduzhir")
+         ],
+         [
+             InlineKeyboardButton(text="Помошь🙋‍♂️",callback_data="help_pressed"),
+             InlineKeyboardButton(text="О проекте❓",callback_data="about_pressed"),
+             InlineKeyboardButton(text="Отзыв🙌",url="https://t.me/vandal_325")
          ]
+
 
      ]
  )
@@ -24,6 +33,14 @@ escape_keyboard = InlineKeyboardMarkup(
     inline_keyboard=[
         [
             InlineKeyboardButton(text="Назад❌",callback_data="escape_pressed")
+        ]
+    ]
+)
+
+escape_keyboard_caption = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Назад❌",callback_data="escape_caption_pressed")
         ]
     ]
 )
